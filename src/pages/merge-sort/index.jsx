e988050be75206c18eccb0fb349f1cffe1bbda10
ashu <ashudeshwal999@gmail.com>
2020-07-06 02:53:13 +0530

    remove useMemo

--- src/pages/merge-sort/index.jsx
+++ src/pages/merge-sort/index.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import ElementList from "../../components/elementList";
 import AlogithmHoc from "../../HOC/Algorithm";
@@ -11,98 +11,95 @@ function MergeSort({
   divKey,
   array,
 }) {
-  const renderMergeSort = useMemo(() => {
-    let count = 0;
-    const merge = (left, right) => {
-      let resultArray = [],
-        leftIndex = 0,
-        rightIndex = 0;
+  let count = 0;
+  const merge = (left, right) => {
+    let resultArray = [],
+      leftIndex = 0,
+      rightIndex = 0;
 
-      while (leftIndex < left.length && rightIndex < right.length) {
-        if (left[leftIndex] < right[rightIndex]) {
-          resultArray.push(left[leftIndex]);
-          leftIndex++;
-        } else {
-          resultArray.push(right[rightIndex]);
-          rightIndex++;
-        }
+    while (leftIndex < left.length && rightIndex < right.length) {
+      if (left[leftIndex] < right[rightIndex]) {
+        resultArray.push(left[leftIndex]);
+        leftIndex++;
+      } else {
+        resultArray.push(right[rightIndex]);
+        rightIndex++;
       }
-      count += left.length + right.length;
+    }
+    count += left.length + right.length;
 
-      return resultArray
-        .concat(left.slice(leftIndex))
-        .concat(right.slice(rightIndex));
-    };
-    const mergeSort = (unsortedArray) => {
-      const tempCount = (count += unsortedArray.length);
-      const countArray = unsortedArray.map(
-        (_, i) => tempCount - unsortedArray.length + i
-      );
+    return resultArray
+      .concat(left.slice(leftIndex))
+      .concat(right.slice(rightIndex));
+  };
+  const mergeSort = (unsortedArray) => {
+    const tempCount = (count += unsortedArray.length);
+    const countArray = unsortedArray.map(
+      (_, i) => tempCount - unsortedArray.length + i
+    );
 
-      if (unsortedArray.length <= 1) {
-        return [
-          unsortedArray,
-          <div className="space-around">
-            <ElementList
-              values={unsortedArray}
-              count={countArray}
-              end={true}
-              speed={speed}
-              currentTimeStamp={currentTimeStamp}
-              setEndTimeStamp={setEndTimeStamp}
-              setStartTimeStamp={setStartTimeStamp}
-            />
-          </div>,
-        ];
-      }
-
-      const middle = Math.ceil(unsortedArray.length / 2);
-      const left = unsortedArray.slice(0, middle);
-      const right = unsortedArray.slice(middle);
-
-      const leftResult = mergeSort(left);
-      const rightResult = mergeSort(right);
-
-      const sortedArray = merge(leftResult[0], rightResult[0]);
-      const newCountArray = unsortedArray.map(
-        (_, i) => count - (unsortedArray.length - i)
-      );
-
+    if (unsortedArray.length <= 1) {
       return [
-        sortedArray,
-        <div className="child">
-          <div className="space-around">
-            <ElementList
-              values={unsortedArray}
-              newValues={sortedArray}
-              count={countArray}
-              newCount={newCountArray}
-              end={false}
-              speed={speed}
-              currentTimeStamp={currentTimeStamp}
-              setEndTimeStamp={setEndTimeStamp}
-              setStartTimeStamp={setStartTimeStamp}
-            />
-          </div>
-          <div
-            className="space-around"
-            style={{ width: `${22 * 3 * unsortedArray.length}px` }}
-          >
-            {leftResult[1]}
-            {rightResult[1]}
-          </div>
+        unsortedArray,
+        <div className="space-around">
+          <ElementList
+            values={unsortedArray}
+            count={countArray}
+            end={true}
+            speed={speed}
+            currentTimeStamp={currentTimeStamp}
+            setEndTimeStamp={setEndTimeStamp}
+            setStartTimeStamp={setStartTimeStamp}
+          />
         </div>,
       ];
-    };
-    return mergeSort(array)[1];
-  }, [array, speed, currentTimeStamp, setEndTimeStamp, setStartTimeStamp]);
+    }
+
+    const middle = Math.ceil(unsortedArray.length / 2);
+    const left = unsortedArray.slice(0, middle);
+    const right = unsortedArray.slice(middle);
+
+    const leftResult = mergeSort(left);
+    const rightResult = mergeSort(right);
+
+    const sortedArray = merge(leftResult[0], rightResult[0]);
+    const newCountArray = unsortedArray.map(
+      (_, i) => count - (unsortedArray.length - i)
+    );
+
+    return [
+      sortedArray,
+      <div className="child">
+        <div className="space-around">
+          <ElementList
+            values={unsortedArray}
+            newValues={sortedArray}
+            count={countArray}
+            newCount={newCountArray}
+            end={false}
+            speed={speed}
+            currentTimeStamp={currentTimeStamp}
+            setEndTimeStamp={setEndTimeStamp}
+            setStartTimeStamp={setStartTimeStamp}
+          />
+        </div>
+        <div
+          className="space-around"
+          style={{ width: `${22 * 3 * unsortedArray.length}px` }}
+        >
+          {leftResult[1]}
+          {rightResult[1]}
+        </div>
+      </div>,
+    ];
+  };
 
   return (
     <React.Fragment>
       <h1>Merge Sort</h1>
       <div className="current" />
       <div key={divKey} className="visRoot merge-sort">
-        {renderMergeSort}
+        {mergeSort(array)[1]}
       </div>
     </React.Fragment>
   );
--- src/pages/quick-sort/index.jsx
+++ src/pages/quick-sort/index.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import ElementList from "../../components/elementList";
 import AlogithmHoc from "../../HOC/Algorithm";
@@ -11,128 +11,125 @@ function QuickSort({
   divKey,
   array,
 }) {
-  const renderQuickSort = useMemo(() => {
-    let count = 0;
-    const quickSort = (items, left, right) => {
-      const partitionResult = partition(items, left, right);
-      const index = partitionResult[0];
-      const centerElement = partitionResult[1];
-      let leftElement;
-      let rightElement;
+  let count = 0;
+  const quickSort = (items, left, right) => {
+    const partitionResult = partition(items, left, right);
+    const index = partitionResult[0];
+    const centerElement = partitionResult[1];
+    let leftElement;
+    let rightElement;
 
-      if (left < index - 1) {
-        leftElement = quickSort(items, left, index - 1);
-      } else if (left === index - 1) {
-        count++;
-        leftElement = (
-          <div className="space-around">
-            <ElementList
-              values={[items[index - 1]]}
-              count={[count]}
-              end={true}
-              speed={speed}
-              currentTimeStamp={currentTimeStamp}
-              setEndTimeStamp={setEndTimeStamp}
-              setStartTimeStamp={setStartTimeStamp}
-            />
-          </div>
-        );
-      }
-
-      if (index < right) {
-        rightElement = quickSort(items, index, right);
-      } else if (index === right) {
-        count++;
-        rightElement = (
-          <div className="space-around">
-            <ElementList
-              values={[items[index]]}
-              count={[count]}
-              end={true}
-              speed={speed}
-              currentTimeStamp={currentTimeStamp}
-              setEndTimeStamp={setEndTimeStamp}
-              setStartTimeStamp={setStartTimeStamp}
-            />
-          </div>
-        );
-      }
-
-      return (
-        <div className="child">
-          <div className="space-around">{centerElement}</div>
-          <div
-            className="space-around"
-            style={{ width: `${22 * 3 * (right - left + 1)}px` }}
-          >
-            {leftElement}
-            {rightElement}
-          </div>
+    if (left < index - 1) {
+      leftElement = quickSort(items, left, index - 1);
+    } else if (left === index - 1) {
+      count++;
+      leftElement = (
+        <div className="space-around">
+          <ElementList
+            values={[items[index - 1]]}
+            count={[count]}
+            end={true}
+            speed={speed}
+            currentTimeStamp={currentTimeStamp}
+            setEndTimeStamp={setEndTimeStamp}
+            setStartTimeStamp={setStartTimeStamp}
+          />
         </div>
       );
-    };
+    }
 
-    const partition = (items, left, right) => {
-      const midIndex = Math.floor((right + left) / 2);
-      const pivot = items[midIndex];
-      let i = left;
-      let j = right;
-      const valuesWithLabel = [...items];
-      valuesWithLabel[midIndex] = (
-        <React.Fragment>
-          {pivot}
-          <span className="pivot-label">Pivot</span>
-        </React.Fragment>
+    if (index < right) {
+      rightElement = quickSort(items, index, right);
+    } else if (index === right) {
+      count++;
+      rightElement = (
+        <div className="space-around">
+          <ElementList
+            values={[items[index]]}
+            count={[count]}
+            end={true}
+            speed={speed}
+            currentTimeStamp={currentTimeStamp}
+            setEndTimeStamp={setEndTimeStamp}
+            setStartTimeStamp={setStartTimeStamp}
+          />
+        </div>
       );
+    }
 
-      const values = valuesWithLabel.slice(left, right + 1);
-      const countArray = values.map((_) => count++);
-      const newCountArray = [];
+    return (
+      <div className="child">
+        <div className="space-around">{centerElement}</div>
+        <div
+          className="space-around"
+          style={{ width: `${22 * 3 * (right - left + 1)}px` }}
+        >
+          {leftElement}
+          {rightElement}
+        </div>
+      </div>
+    );
+  };
 
-      while (i <= j) {
-        while (items[i] < pivot) {
-          newCountArray[i] = count++;
-          i++;
-        }
-        while (items[j] > pivot) {
-          newCountArray[j] = count++;
-          j--;
-        }
-        if (i <= j) {
-          [items[i], items[j]] = [items[j], items[i]];
-          newCountArray[i] = count;
-          newCountArray[j] = count;
-          count++;
-          i++;
-          j--;
-        }
+  const partition = (items, left, right) => {
+    const midIndex = Math.floor((right + left) / 2);
+    const pivot = items[midIndex];
+    let i = left;
+    let j = right;
+    const valuesWithLabel = [...items];
+    valuesWithLabel[midIndex] = (
+      <React.Fragment>
+        {pivot}
+        <span className="pivot-label">Pivot</span>
+      </React.Fragment>
+    );
+
+    const values = valuesWithLabel.slice(left, right + 1);
+    const countArray = values.map((_) => count++);
+    const newCountArray = [];
+
+    while (i <= j) {
+      while (items[i] < pivot) {
+        newCountArray[i] = count++;
+        i++;
       }
-      const newValues = items.slice(left, right + 1);
+      while (items[j] > pivot) {
+        newCountArray[j] = count++;
+        j--;
+      }
+      if (i <= j) {
+        [items[i], items[j]] = [items[j], items[i]];
+        newCountArray[i] = count;
+        newCountArray[j] = count;
+        count++;
+        i++;
+        j--;
+      }
+    }
+    const newValues = items.slice(left, right + 1);
 
-      return [
-        i,
-        <ElementList
-          values={values}
-          newValues={newValues}
-          count={countArray}
-          newCount={newCountArray.slice(left, right + 1)}
-          end={false}
-          speed={speed}
-          currentTimeStamp={currentTimeStamp}
-          setEndTimeStamp={setEndTimeStamp}
-          setStartTimeStamp={setStartTimeStamp}
-        />,
-      ];
-    };
-    return quickSort([...array], 0, array.length - 1);
-  }, [array, speed, currentTimeStamp, setEndTimeStamp, setStartTimeStamp]);
+    return [
+      i,
+      <ElementList
+        values={values}
+        newValues={newValues}
+        count={countArray}
+        newCount={newCountArray.slice(left, right + 1)}
+        end={false}
+        speed={speed}
+        currentTimeStamp={currentTimeStamp}
+        setEndTimeStamp={setEndTimeStamp}
+        setStartTimeStamp={setStartTimeStamp}
+      />,
+    ];
+  };
 
   return (
     <React.Fragment>
       <h1>Quick Sort</h1>
       <div className="current" />
       <div key={divKey} className="visRoot quick-sort">
-        {renderQuickSort}
+        {quickSort([...array], 0, array.length - 1)}
       </div>
     </React.Fragment>
   );
